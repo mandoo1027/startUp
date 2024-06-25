729735b4d77435ed697598eb66168a8392451ce3
김경일 <mandoo1027@gmail.com>
2024-06-25 22:45:47 +0900

    test3추가

--- components/Board/ListComp.vue
+++ components/Board/ListComp.vue
@@ -11,6 +11,9 @@ const CheckCount = () =>{
     const params = {cnt : "1"}
     emit('update', params)
 }
+const test3 = ref(0);
+const test4 = ref(0);
+const test5 = ref(0);
 </script>
 
 <style lang="scss" scoped>
