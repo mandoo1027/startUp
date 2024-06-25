750fbf61c0efda433be51c0ab586dd9ed7d0e781
조성인 <chosungin@bycho-MacBookAir.local>
2024-06-25 23:08:46 +0900

    defineexpos연습

--- pages/Board/boardList.vue
+++ pages/Board/boardList.vue
@@ -2,13 +2,22 @@
     <div>
         -게시판 목록-
         갯수 : {{count}}
-        <BoardListComp @update="update"></BoardListComp>
+        <BoardListComp @update="update1" ref="boardChid"></BoardListComp>
+        <input type="button" value="expos" @click="exposTest">
+
     </div>
 </template>
 
 <script setup>
+const boardChid = ref(null);
+
+const exposTest = () => {
+    console.log(boardChid.value.a1);
+    boardChid.value.a2()
+}
+
 let count = ref(0)
-const update = (params) => {
+const update1 = (params) => {
     
     count.value = params.cnt
     console.log(count.value);
